40d719af42707c6cd38c2346eb3a56856622d3ce
leroykim <leroy.kim@umbc.edu>
2022-09-23 17:28:12 -0400

    Add provider convert method

--- README.md
+++ README.md
@@ -3,9 +3,25 @@ Semantic web representation for the Synthea.
 
 ## Usage
 ```python
-from synthea_rdf import patient
+from synthea_rdf import patient, encounter, observation, organization, provider
 
 patient_df = pd.read_csv("csv/patients.csv", dtype={'ZIP':str})
 for index, row in patient_df.iterrows():
     patient.convert(g, row)
+
+encounter_df = pd.read_csv("csv/encounters.csv")
+for index, row in encounter_df.iterrows():
+    encounter.convert(g, row)
+
+observation_df = pd.read_csv("csv/observations.csv")
+for index, row in observation_df.iterrows():
+    observation.convert(g, row, index)
+
+organization_df = pd.read_csv("csv/organizations.csv")
+for index, row in organization_df.iterrows():
+    organization.convert(g, row)
+
+provider_df = pd.read_csv("csv/providers.csv")
+for index, row in provider_df.iterrows():
+    provider.convert(g, row)
 ```
---
+++ synthea_rdf/provider.py
@@ -0,0 +1,19 @@
+from rdflib.namespace import RDF
+
+from .helper import organization_uri, string_literal, int_literal, organization_uri, provider_uri
+from .setting import SYN
+
+def convert(graph, row):
+    provider = provider_uri(row['Id'])
+    graph.add((provider, RDF.type, SYN.Provider))
+    graph.add((provider, SYN.belongsTo, organization_uri(row['ORGANIZATION'])))
+    graph.add((provider, SYN.name, string_literal(row['NAME'])))
+    graph.add((provider, SYN.gender, string_literal(row['GENDER'])))
+    graph.add((provider, SYN.speciality, string_literal(row['SPECIALITY'])))
+    graph.add((provider, SYN.name, string_literal(row['NAME'])))
+    graph.add((provider, SYN.address, string_literal(row['ADDRESS'])))
+    graph.add((provider, SYN.city, string_literal(row['CITY'])))
+    graph.add((provider, SYN.state, string_literal(row['STATE'])))
+    graph.add((provider, SYN.zip, string_literal(row['ZIP'])))
+    graph.add((provider, SYN.utilization, int_literal(row['UTILIZATION'])))
+    
