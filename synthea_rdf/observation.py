746930a92df680cc38440a4e0d270673f940d535
leroykim <leroy.kim@umbc.edu>
2022-09-27 11:19:08 -0400

    Fix convert method interface

--- synthea_rdf/observation.py
+++ synthea_rdf/observation.py
@@ -3,15 +3,16 @@ from rdflib.namespace import RDF
 from .helper import datetime_literal, string_literal, patient_uri, observation_uri, encounter_uri
 from .setting import SYN
 
-def convert(graph, row, index):
-    observation = observation_uri(index)
-    graph.add((observation, RDF.type, SYN.Observation))
-    graph.add((observation, SYN.date, datetime_literal(row['DATE'])))
-    graph.add((observation, SYN.observedPatient, patient_uri(row['PATIENT'])))
-    graph.add((observation, SYN.isFromEncounter, encounter_uri(row['ENCOUNTER'])))
-    graph.add((observation, SYN.category, string_literal(row['CATEGORY'])))
-    graph.add((observation, SYN.observation_code, string_literal(row['CODE'])))
-    graph.add((observation, SYN.observation_description, string_literal(row['DESCRIPTION'])))
-    graph.add((observation, SYN.value, string_literal(row['VALUE'])))
-    graph.add((observation, SYN.units, string_literal(row['UNITS'])))
-    graph.add((observation, SYN.type, string_literal(row['TYPE'])))
+def convert(graph, observation_df):
+    for index, row in observation_df.iterrows():
+        observation = observation_uri(index)
+        graph.add((observation, RDF.type, SYN.Observation))
+        graph.add((observation, SYN.date, datetime_literal(row['DATE'])))
+        graph.add((observation, SYN.observedPatient, patient_uri(row['PATIENT'])))
+        graph.add((observation, SYN.isFromEncounter, encounter_uri(row['ENCOUNTER'])))
+        graph.add((observation, SYN.category, string_literal(row['CATEGORY'])))
+        graph.add((observation, SYN.observation_code, string_literal(row['CODE'])))
+        graph.add((observation, SYN.observation_description, string_literal(row['DESCRIPTION'])))
+        graph.add((observation, SYN.value, string_literal(row['VALUE'])))
+        graph.add((observation, SYN.units, string_literal(row['UNITS'])))
+        graph.add((observation, SYN.type, string_literal(row['TYPE'])))
--- synthea_rdf/organization.py
+++ synthea_rdf/organization.py
@@ -3,14 +3,15 @@ from rdflib.namespace import RDF
 from .helper import organization_uri, string_literal, int_literal, float_literal, organization_uri
 from .setting import SYN
 
-def convert(graph, row):
-    organization = organization_uri(row['Id'])
-    graph.add((organization, RDF.type, SYN.Organization))
-    graph.add((organization, SYN.name, string_literal(row['NAME'])))
-    graph.add((organization, SYN.address, string_literal(row['ADDRESS'])))
-    graph.add((organization, SYN.city, string_literal(row['CITY'])))
-    graph.add((organization, SYN.state, string_literal(row['STATE'])))
-    graph.add((organization, SYN.zip, string_literal(row['ZIP'])))
-    graph.add((organization, SYN.phone, string_literal(row['PHONE'])))
-    graph.add((organization, SYN.revenue, float_literal(row['REVENUE'])))
-    graph.add((organization, SYN.utilization, int_literal(row['UTILIZATION'])))
+def convert(graph, organization_df):
+    for _, row in organization_df.iterrows():
+        organization = organization_uri(row['Id'])
+        graph.add((organization, RDF.type, SYN.Organization))
+        graph.add((organization, SYN.name, string_literal(row['NAME'])))
+        graph.add((organization, SYN.address, string_literal(row['ADDRESS'])))
+        graph.add((organization, SYN.city, string_literal(row['CITY'])))
+        graph.add((organization, SYN.state, string_literal(row['STATE'])))
+        graph.add((organization, SYN.zip, string_literal(row['ZIP'])))
+        graph.add((organization, SYN.phone, string_literal(row['PHONE'])))
+        graph.add((organization, SYN.revenue, float_literal(row['REVENUE'])))
+        graph.add((organization, SYN.utilization, int_literal(row['UTILIZATION'])))
