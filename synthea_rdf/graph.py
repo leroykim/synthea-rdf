2b48e373adb62058e9625ba0eb1873b207da25f3
leroykim <leroy.kim@umbc.edu>
2022-09-28 10:28:20 -0400

    Refactoring done

---
+++ synthea_rdf/graph.py
@@ -0,0 +1,88 @@
+from pathlib import Path
+from re import S
+from rdflib import Graph, Literal
+from .resource import Encounter, Observation, Organization, Patient, Provider
+from .setting import SYN
+
+class GraphBuilder():
+    def __init__(self, persistence=None):
+        if persistence is None:
+            self.graph = Graph()
+        elif persistence == "sqlite":
+            self.__init_sqlite()
+        
+        self.patient_df = None
+        self.encounter_df = None
+        self.observation_df = None
+        self.organization_df = None
+        self.provider_df = None
+
+    def build(self, model_path=None):
+        if model_path is not None:
+            self.__set_model(model_path)
+        else:
+            print("Model path required!")
+            return
+
+        if self.patient_df is None:
+            print("Patient data frame file must be provided!")
+            return
+
+        self.__convert_patient()
+        self.__convert_encounter()
+        self.__convert_observation()
+        self.__convert_organization()
+        self.__convert_provider()
+
+        return self.graph
+
+    def __init_sqlite(self):
+        self.graph = Graph('SQLAlchemy', identifier='synthea_graph')
+        persistence_path = Path('.')/'persistence'
+        persistence_path.mkdir(exist_ok=True)
+        dbfile_path = persistence_path/'synthea_patient.sqlite'
+        dburi = Literal(f"sqlite:///{dbfile_path}")
+        if not dbfile_path.is_file():
+            self.graph.open(dburi, create=True)
+        else:
+            self.graph.open(dburi)
+    
+    def __set_model(self, model_path):
+        self.graph.parse(model_path, format="n3")
+        self.graph.bind("syn", SYN)
+        print(f"Model has {len(self.graph)} triples.")
+
+    def __convert_patient(self):
+        if self.patient_df is not None:
+            patient = Patient(self.patient_df)
+            patient.convert(self.graph)
+        else:
+            print("Patient_df is not set.")
+
+    def __convert_encounter(self):
+        if self.encounter_df is not None:
+            encounter = Encounter(self.encounter_df)
+            encounter.convert(self.graph)
+        else:
+            print("Encounter_df is not set.")
+    
+    def __convert_observation(self):
+        if self.observation_df is not None:
+            observation = Observation(self.observation_df)
+            observation.convert(self.graph)
+        else:
+            print("Observation_df is not set.")
+    
+    def __convert_organization(self):
+        if self.organization_df is not None:
+            organization = Organization(self.organization_df)
+            organization.convert(self.graph)
+        else:
+            print("Organization_df is note set.")
+
+    def __convert_provider(self):
+        if self.provider_df is not None:
+            provider = Provider(self.provider_df)
+            provider.convert(self.graph)
+        else:
+            print("Provider_df is not set.")
---
+++ synthea_rdf/resource.py
@@ -0,0 +1,223 @@
+from abc import ABC, abstractmethod
+
+from datetime import datetime
+
+from alive_progress import alive_bar
+import pandas as pd
+from rdflib import Literal, URIRef
+from rdflib.namespace import RDF, XSD
+
+from .setting import SYN
+
+class Resource(ABC):
+    @abstractmethod
+    def __init__(self, df):
+        self.__resource_df = df
+
+    @property
+    @abstractmethod
+    def resource_df(self):
+        pass
+
+    @resource_df.setter
+    @abstractmethod
+    def resource_df(self, resource_df):
+        pass
+
+    @abstractmethod
+    def convert(self, graph):
+        pass
+
+class Encounter(Resource):
+    def __init__(self, df):
+        self.__resource_df = df
+
+    @property
+    def resource_df(self):
+        return self.__resource_df
+
+    @resource_df.setter
+    def resource_df(self, value):
+        self.__resource_df = value
+
+    def convert(self, graph):
+        with alive_bar(self.__resource_df.shape[0], force_tty=True, title='Encounter Conversion') as bar:
+            for _, row in self.__resource_df.iterrows():
+                encounter = encounter_uri(row['Id'])
+                graph.add((encounter, RDF.type, SYN.Encounter))
+                graph.add((encounter, SYN.start_time, datetime_literal(row['START'])))
+                graph.add((encounter, SYN.stop_time, datetime_literal(row['STOP'])))
+                graph.add((encounter, SYN.encounterdPatient, patient_uri(row['PATIENT'])))
+                graph.add((encounter, SYN.assessedIn, organization_uri(row['ORGANIZATION'])))
+                graph.add((encounter, SYN.assessedBy, provider_uri(row['PROVIDER'])))
+                graph.add((encounter, SYN.paidBy, payer_uri(row['PAYER'])))
+                graph.add((encounter, SYN.encounter_class, string_literal(row['ENCOUNTERCLASS'])))
+                graph.add((encounter, SYN.encounter_code, string_literal(row['CODE'])))
+                graph.add((encounter, SYN.encounter_description, string_literal(row['DESCRIPTION'])))
+                graph.add((encounter, SYN.base_encounter_cost, float_literal(row['BASE_ENCOUNTER_COST'])))
+                graph.add((encounter, SYN.total_claim_cost, float_literal(row['TOTAL_CLAIM_COST'])))
+                graph.add((encounter, SYN.payer_coverage, float_literal(row['PAYER_COVERAGE'])))
+                graph.add((encounter, SYN.reason_code, string_literal(row['REASONCODE'])))
+                graph.add((encounter, SYN.reason_description, string_literal(row['REASONDESCRIPTION'])))
+                bar()
+
+class Observation(Resource):
+    def __init__(self, df):
+        self.__resource_df = df
+
+    @property
+    def resource_df(self):
+        return self.__resource_df
+
+    @resource_df.setter
+    def resource_df(self, value):
+        self.__resource_df = value
+
+    def convert(self, graph):
+        with alive_bar(self.__resource_df.shape[0], force_tty=True, title='Obvervation Conversion') as bar:
+            for index, row in self.__resource_df.iterrows():
+                observation = observation_uri(index)
+                graph.add((observation, RDF.type, SYN.Observation))
+                graph.add((observation, SYN.date, datetime_literal(row['DATE'])))
+                graph.add((observation, SYN.observedPatient, patient_uri(row['PATIENT'])))
+                graph.add((observation, SYN.isFromEncounter, encounter_uri(row['ENCOUNTER'])))
+                graph.add((observation, SYN.category, string_literal(row['CATEGORY'])))
+                graph.add((observation, SYN.observation_code, string_literal(row['CODE'])))
+                graph.add((observation, SYN.observation_description, string_literal(row['DESCRIPTION'])))
+                graph.add((observation, SYN.value, string_literal(row['VALUE'])))
+                graph.add((observation, SYN.units, string_literal(row['UNITS'])))
+                graph.add((observation, SYN.type, string_literal(row['TYPE'])))
+                bar()
+
+class Organization(Resource):
+    def __init__(self, df):
+        self.__resource_df = df
+
+    @property
+    def resource_df(self):
+        return self.__resource_df
+
+    @resource_df.setter
+    def resource_df(self, value):
+        self.__resource_df = value
+
+    def convert(self, graph):
+        with alive_bar(self.__resource_df.shape[0], force_tty=True, title='Organization Conversion') as bar:
+            for _, row in self.__resource_df.iterrows():
+                organization = organization_uri(row['Id'])
+                graph.add((organization, RDF.type, SYN.Organization))
+                graph.add((organization, SYN.name, string_literal(row['NAME'])))
+                graph.add((organization, SYN.address, string_literal(row['ADDRESS'])))
+                graph.add((organization, SYN.city, string_literal(row['CITY'])))
+                graph.add((organization, SYN.state, string_literal(row['STATE'])))
+                graph.add((organization, SYN.zip, string_literal(row['ZIP'])))
+                graph.add((organization, SYN.phone, string_literal(row['PHONE'])))
+                graph.add((organization, SYN.revenue, float_literal(row['REVENUE'])))
+                graph.add((organization, SYN.utilization, int_literal(row['UTILIZATION'])))
+                bar()
+
+class Patient(Resource):
+    def __init__(self, df):
+        self.__resource_df = df
+
+    @property
+    def resource_df(self):
+        return self.__resource_df
+
+    @resource_df.setter
+    def resource_df(self, value):
+        self.__resource_df = value
+
+    def convert(self, graph):
+        with alive_bar(self.__resource_df.shape[0], force_tty=True, title='Patient Conversion') as bar:
+            for _, row in self.__resource_df.iterrows():
+                patient =  patient_uri(row['Id'])
+                graph.add((patient, RDF.type, SYN.Patient)) # type
+                graph.add((patient, SYN.id, string_literal(row['Id']))) # id
+                graph.add((patient, SYN.birthdate, date_literal(row['BIRTHDATE']))) # birthdate
+                if pd.notnull(row['DEATHDATE']):
+                    graph.add(patient, SYN.DEATHDATE, date_literal(row['DEATHDATE'])) # deathdate
+                graph.add((patient, SYN.ssn, string_literal(row['SSN']))) # ssn
+                graph.add((patient, SYN.drivers, string_literal(row['DRIVERS']))) # drivers license
+                graph.add((patient, SYN.passport, string_literal(row['PASSPORT'])))
+                graph.add((patient, SYN.firstname, string_literal(row['FIRST'])))
+                graph.add((patient, SYN.lastname, string_literal(row['LAST'])))
+                graph.add((patient, SYN.marital, string_literal(row['MARITAL'])))
+                graph.add((patient, SYN.race, string_literal(row['RACE'])))
+                graph.add((patient, SYN.ethnicity, string_literal(row['ETHNICITY'])))
+                graph.add((patient, SYN.gender, string_literal(row['GENDER'])))
+                graph.add((patient, SYN.birthplace, string_literal(row['BIRTHPLACE'])))
+                graph.add((patient, SYN.address, string_literal(row['ADDRESS'])))
+                graph.add((patient, SYN.city, string_literal(row['CITY'])))
+                graph.add((patient, SYN.state, string_literal(row['STATE'])))
+                graph.add((patient, SYN.county, string_literal(row['COUNTY'])))
+                graph.add((patient, SYN.zip, string_literal(row['ZIP'])))
+                graph.add((patient, SYN.healthcare_coverage, float_literal(row['HEALTHCARE_COVERAGE'])))
+                graph.add((patient, SYN.healthcare_expenses, float_literal(row['HEALTHCARE_EXPENSES'])))
+                graph.add((patient, SYN.income, int_literal(row['INCOME'])))
+                bar()
+
+class Provider(Resource):
+    def __init__(self, df):
+        self.__resource_df = df
+
+    @property
+    def resource_df(self):
+        return self.__resource_df
+
+    @resource_df.setter
+    def resource_df(self, value):
+        self.__resource_df = value
+
+    def convert(self, graph):
+        with alive_bar(self.__resource_df.shape[0], force_tty=True, title='Provider Conversion') as bar:
+            for _, row in self.__resource_df.iterrows():
+                provider = provider_uri(row['Id'])
+                graph.add((provider, RDF.type, SYN.Provider))
+                graph.add((provider, SYN.belongsTo, organization_uri(row['ORGANIZATION'])))
+                graph.add((provider, SYN.name, string_literal(row['NAME'])))
+                graph.add((provider, SYN.gender, string_literal(row['GENDER'])))
+                graph.add((provider, SYN.speciality, string_literal(row['SPECIALITY'])))
+                graph.add((provider, SYN.name, string_literal(row['NAME'])))
+                graph.add((provider, SYN.address, string_literal(row['ADDRESS'])))
+                graph.add((provider, SYN.city, string_literal(row['CITY'])))
+                graph.add((provider, SYN.state, string_literal(row['STATE'])))
+                graph.add((provider, SYN.zip, string_literal(row['ZIP'])))
+                graph.add((provider, SYN.utilization, int_literal(row['UTILIZATION'])))
+                bar()
+    
+
+# Literal helper methods
+def date_literal(date):
+    return Literal(datetime.strptime(date,'%Y-%m-%d'), datatype=XSD.dateTime)
+
+def datetime_literal(date):
+    return Literal(datetime.strptime(date,"%Y-%m-%dT%H:%M:%SZ"), datatype=XSD.dateTime)
+
+def string_literal(string):
+    return Literal(str(string), datatype=XSD.string)
+
+def int_literal(string):
+    return Literal(str(string), datatype=XSD.int)
+
+def float_literal(string):
+    return Literal(str(string), datatype=XSD.float)
+
+# URI helper methods
+def patient_uri(patient_id):
+    return URIRef(f'patient_{patient_id}')
+
+def encounter_uri(encounter_id):
+    return URIRef(f"encounter_{encounter_id}")
+
+def organization_uri(organization_id):
+    return URIRef(f'organization_{organization_id}')
+
+def provider_uri(provider_id):
+    return URIRef(f'provider_{provider_id}')
+
+def payer_uri(payer_id):
+    return URIRef(f'payer_{payer_id}')
+
+def observation_uri(observation_id):
+    return URIRef(f'observation_{observation_id}')
