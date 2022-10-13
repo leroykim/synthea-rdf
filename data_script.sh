417289c2f0d9acda1ecd043714efafd3f3814740
leroykim <leroy.kim@umbc.edu>
2022-10-12 20:34:17 -0400

    Add options in data script

--- data_script.sh
+++ data_script.sh
@@ -4,7 +4,7 @@ CSV_DIR="${CURRENT_DIR}/csv"
 cd ~/synthea
 for (( i=1 ; i<=$1 ; i++ ));
 do
-    ~/synthea/run_synthea -p $2;
-    cp -a ~/synthea/output/csv/. ~/synthea_rdf/csv/patient_$2_bin_$i
+    ~/synthea/run_synthea -p $2 -m $3 $4;
+    cp -a ~/synthea/output/csv/. ~/synthea_rdf/csv/$4_$3_patient_$2_bin_$i
 done
 cd $CURRENT_DIR
