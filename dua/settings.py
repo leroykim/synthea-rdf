a29a3fe4f0f00899676cdb0a393e818de2ecedf1
leroykim <leroy.kim@umbc.edu>
2023-04-04 14:47:58 -0400

    Update dua generator

--- dua/settings.py
+++ dua/settings.py
@@ -6,18 +6,41 @@ SYNTHEA_IRI = "https://knacc.umbc.edu/leroy/ontologies/synthea#"
 DUA = Namespace(DUA_IRI)
 SYN = Namespace(SYNTHEA_IRI)
 
-DUA_COLS = ['dataCustodian','recipient', 'requestedData', # Basic Information
-'permittedUseOrDisclosure', 'terms', 'terminationCause', 'terminationEffect',  # TermAndTermination
-'dataSecurityPlanAccess', 'dataSecurityPlanProtection', 'dataSecurityPlanStorage']  # Data_Security_Plan
-DUA_COUNT = 7
+DUA_COLS = [
+    "dataCustodian",
+    "recipient",
+    "requestedData",  # Basic Information
+    "permittedUseOrDisclosure",
+    "terms",
+    "terminationCause",
+    "terminationEffect",  # TermAndTermination
+    "dataSecurityPlanAccess",
+    "dataSecurityPlanProtection",
+    "dataSecurityPlanStorage",
+]  # Data_Security_Plan
 
 PERMITTED_USE_OR_DISCLOSURE = [
-    'treatment', 'payment', 'health_care_operations', 'notification', 'public_health', 'limited_data_set']
+    "treatment",
+    "payment",
+    "health_care_operations",
+    "notification",
+    "public_health",
+    "limited_data_set",
+]
 
 
-DATA_CLASSES = ['Condition', 'Encounter', 'Immunization', 'Medication', 'Observation',
-                'Organization', 'Patient', 'Payer', 'Procedure', 'Provider']
+DATA_CLASSES = [
+    "Condition",
+    "Encounter",
+    "Immunization",
+    "Medication",
+    "Observation",
+    "Organization",
+    "Patient",
+    "Payer",
+    "Procedure",
+    "Provider",
+]
 DATA_CLASS_SAMPLE_SIZE = 3
 
 RANDOM_SEED = 7
-SAVE_PATH = "./csv/dua/dua.csv"
